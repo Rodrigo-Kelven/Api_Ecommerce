965141b50ef02d0462cb9c89d3296c3dffa074d9
Kelven <kelvenr40@gmail.com>
2025-01-22 21:50:04 -0300

    atualizando o databases

--- ecommerce/databases/ecommerce_config/database.py
+++ ecommerce/databases/ecommerce_config/database.py
@@ -1,9 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Definindo o caminho do banco de dados SQLite
+db_path = "./ecommerce/databases/DB/ecommerce.db"
+
+# Verificar se a pasta existe, caso contrário, criar
+db_directory = os.path.dirname(db_path)  # Pega a pasta do caminho do DB
+if not os.path.exists(db_directory):
+    os.makedirs(db_directory)  # Cria o diretório se não existir
+
 # URL do banco de dados SQLite dentro da pasta 'databases'
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ecommerce/databases/DB/ecommerce.db"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
 
 # Criando o engine para conectar ao banco de dados
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
