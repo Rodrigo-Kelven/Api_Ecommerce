39a910bab8fb5b9b32f50058cb6050f39d3f6db7
Kelven <kelvenr40@gmail.com>
2025-01-23 23:46:43 -0300

    anotacoes sobre db's

--- ecommerce/databases/ecommerce_config/database.py
+++ ecommerce/databases/ecommerce_config/database.py
@@ -4,15 +4,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Definindo o caminho do banco de dados SQLite
-db_path = "./ecommerce/databases/DB_ecommerce/ecommerce.db"
+db_ecommerce_path = "./ecommerce/databases/DB_ecommerce/ecommerce.db" # -> banco de dados dos produtos
+# db_path = "./ecommerce/databases/DB_users/users_ecommerce.db" -> banco de dados de usuarios 
 
 # Verificar se a pasta existe, caso contrário, criar
-db_directory = os.path.dirname(db_path)  # Pega a pasta do caminho do DB
+db_directory = os.path.dirname(db_ecommerce_path)  # Pega a pasta do caminho do DB
 if not os.path.exists(db_directory):
     os.makedirs(db_directory)  # Cria o diretório se não existir
 
 # URL do banco de dados SQLite dentro da pasta 'databases'
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_ecommerce_path}"
 
 # Criando o engine para conectar ao banco de dados
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
