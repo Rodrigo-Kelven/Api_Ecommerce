e1ccd28c19df5eefe80ca55fd9b23b216ecc163d
Kelven <kelvenr40@gmail.com>
2025-01-30 16:34:32 -0300

    deixando o container mais leve, e criando um script para automatizar o building da images e o up do container

--- ecommerce/script.sh
+++ ecommerce/script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-fastapi dev main.py --reload --port 8000
+docker build -t ecommerce-app .
+# se quiser que rode em background enquanto faz outras coisas
+#docker run -di --name  container_ecommerce-app -p 8000:8000 ecommerce-app
+
+# se quiser que o container suma ao parar a aplicacao
+#docker run --rm --name container_ecommerce-app -p 8000:8000 ecommerce-app
+
+docker run --name container_ecommerce-app -p 8000:8000 ecommerce-app
