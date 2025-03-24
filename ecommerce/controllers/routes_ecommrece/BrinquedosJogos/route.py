3f30f762ecbbd7618dec6ad76f5678c0d17c7965
Kelven <kelvenr40@gmail.com>
2025-03-23 23:59:16 -0300

    add refatoracao de services in Brinquedos e Jogos

--- ecommerce/controllers/routes_ecommrece/BrinquedosJogos/route.py
+++ ecommerce/controllers/routes_ecommrece/BrinquedosJogos/route.py
@@ -53,7 +53,7 @@ async def list_products(
     description="List search products",
     name="Route search products"
 )
-def read_products(
+async def read_products(
     category: str = Query(None, description="Filtrar por categoria"),
     min_price: float = Query(None, description="Filtrar por preço mínimo"),
     max_price: float = Query(None, description="Filtrar por preço máximo"),
@@ -66,47 +66,10 @@ def read_products(
     limit: int = 10,
     db: Session = Depends(get_db)
 ):
-    query = db.query(Product_Brinquedos_Jogos)
-
-    # Aplicar filtros se fornecidos
-    # explicacao: ecommerce/databases/ecommerce_config/database.py -> linha 80
-    if name:
-        query = query.filter(Product_Brinquedos_Jogos.name.ilike(f"%{name}%"))
-
-    if category:# Usando LIKE para categoria
-        query = query.filter(Product_Brinquedos_Jogos.category.ilike(f"%{category}%"))
-
-    if stars:# Usando LIKE para stars
-        query = query.filter(Product_Brinquedos_Jogos.stars >= stars)
-    
-    if color:# Usando LIKE para cor
-        query = query.filter(Product_Brinquedos_Jogos.color.ilike(f"%{color}%"))
-
-    if details:# Usando LIKE para detalhes
-        query = query.filter(Product_Brinquedos_Jogos.details.ilike(f"%{details}%"))
-
-    if size:# Usando LIKE para tamanho
-        query = query.filter(Product_Brinquedos_Jogos.size.ilike(f"%{size}%"))
-
-
-    if min_price is not None:
-        query = query.filter(Product_Brinquedos_Jogos.price >= min_price)
-
-    if max_price is not None:
-        query = query.filter(Product_Brinquedos_Jogos.price <= max_price)
-
-    
-    products = query.offset(skip).limit(limit).all()  # Usando o modelo SQLAlchemy
-
-    if products:
-        logger.info(msg="Produtos de brinquedo e jogos listados!")
-        products_listed = [Product_Brinquedos_Jogos.from_orm(product) for product in products]
-        return products_listed
-
-    logger.info(msg="Nenhum produto de moda encontrado!")
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto de brinquedo e jogos encontrado!")
-
-
+    return await Servico_Brinquedos_Jogos.get_products_with_params(
+        db, name, category, stars, color, details,
+        size, min_price, max_price, skip, limit
+    )
 
 @route_brinquedos_jogos.get(
     path="/category/brinquedos-jogos/{product_id}",
@@ -120,44 +83,7 @@ async def searchProduct_id(
     product_id: str,
     db: Session = Depends(get_db)
 ):
-    product_data = redis_client.get(f"produto_brinquedos_jogos:{product_id}")
-
-    if product_data:
-        logger.info(msg="Produto retornado do Redis!")
-        return json.loads(product_data)
-
-
-    products = db.query(Product_Brinquedos_Jogos).filter(Product_Brinquedos_Jogos.id == product_id).first()
-    
-
-    if products:
-        logger.info(msg="Produto encontrado no banco de dados!")
-        product = Product_Brinquedos_Jogos.from_orm(products)
-
-        product_data = {
-            "id": products.id,
-            "name": products.name,
-            "description": products.description,
-            "price": products.price,
-            "quantity": products.quantity,
-            "tax": products.tax,
-            "stars": products.stars,
-            "color": products.color,
-            "size": products.size,
-            "details": products.details,
-            "category": 'Brinquedos_Jogos'
-        }
-        logger.info(msg="Produto inserido no redis!")
-        # Armazena no Redis com um tempo de expiração de 15 horas (54000 segundos)
-        redis_client.setex(f"produto_brinquedos_jogos:{products.id}", 54000, json.dumps(product_data))
-        logger.info(msg="Produto armazenado no Redis com expiração de 15 horas.")
-        # retorna do db
-        return product
-    
-    if not products:
-        logger.info(msg="Produto nao encontrado!")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
-
+    return await Servico_Brinquedos_Jogos.get_product_ID(product_id, db)
 
 @route_brinquedos_jogos.delete(
     path="/category/brinquedos-jogos/{product_id}",
@@ -170,20 +96,7 @@ async def delete_product(
     product_id: str,
     db: Session = Depends(get_db)
 ):
-    product_delete = db.query(Product_Brinquedos_Jogos).filter(Product_Brinquedos_Jogos.id == product_id).first()
-    
-    if product_delete:
-        logger.info(msg="Produto encontrado!")
-        db.delete(product_delete)
-        db.commit()
-        print("Produto deletado!!")
-        return f"Product with {product_id} removed succesfull"
-    
-    if product_delete is None:
-        logger.info(msg="Produto nao encontrado!")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
-
-
+    return await Servico_Brinquedos_Jogos.delete_product_ID(product_id, db)
 
 @route_brinquedos_jogos.put(
     path="/category/brinquedos-jogos/{product_id}",
@@ -198,24 +111,4 @@ async def update_products(
     product_data: ProductBase = Body(embed=True),
     db: Session = Depends(get_db),
 ):
-    product = db.query(Product_Brinquedos_Jogos).filter(Product_Brinquedos_Jogos.id == product_id).first()
-    
-    if product:
-        # Atualiza os campos do produto com os dados recebidos
-        for key, value in product_data.dict().items():
-            setattr(product, key, value)
-
-        # Corrige o valor da categoria se necessário
-        #product.category = "Beleza_e_cuidados"  
-
-        # Salva as alterações no banco de dados
-        db.commit()
-        db.refresh(product)
-        return product
-
-    
-    # Verifica se o produto existe
-    if product is None:
-        logger.info(msg="Produto nao encontrado!")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
-    
+    return await Servico_Brinquedos_Jogos.update_product_ID(product_id, product_data, db)
--- ecommerce/controllers/services/services_brinquedos.py
+++ ecommerce/controllers/services/services_brinquedos.py
@@ -33,3 +33,131 @@ class Servico_Brinquedos_Jogos:
         if not products:
             logger.info(msg="Nenhum produto inserido!")
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto inserido!")
+        
+
+    @staticmethod
+    async def get_products_with_params(
+        db, name, category, stars, color, details,
+        size, min_price, max_price, skip, limit
+    ):
+        query = db.query(Product_Brinquedos_Jogos)
+
+        # Aplicar filtros se fornecidos
+        # explicacao: ecommerce/databases/ecommerce_config/database.py -> linha 80
+        if name:
+            query = query.filter(Product_Brinquedos_Jogos.name.ilike(f"%{name}%"))
+
+        if category:# Usando LIKE para categoria
+            query = query.filter(Product_Brinquedos_Jogos.category.ilike(f"%{category}%"))
+
+        if stars:# Usando LIKE para stars
+            query = query.filter(Product_Brinquedos_Jogos.stars >= stars)
+        
+        if color:# Usando LIKE para cor
+            query = query.filter(Product_Brinquedos_Jogos.color.ilike(f"%{color}%"))
+
+        if details:# Usando LIKE para detalhes
+            query = query.filter(Product_Brinquedos_Jogos.details.ilike(f"%{details}%"))
+
+        if size:# Usando LIKE para tamanho
+            query = query.filter(Product_Brinquedos_Jogos.size.ilike(f"%{size}%"))
+
+
+        if min_price is not None:
+            query = query.filter(Product_Brinquedos_Jogos.price >= min_price)
+
+        if max_price is not None:
+            query = query.filter(Product_Brinquedos_Jogos.price <= max_price)
+
+        
+        products = query.offset(skip).limit(limit).all()  # Usando o modelo SQLAlchemy
+
+        if products:
+            logger.info(msg="Produtos de brinquedo e jogos listados!")
+            products_listed = [Product_Brinquedos_Jogos.from_orm(product) for product in products]
+            return products_listed
+
+        logger.info(msg="Nenhum produto de moda encontrado!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto de brinquedo e jogos encontrado!")
+    
+
+    @staticmethod
+    async def get_product_ID(product_id, db):
+        product_data = redis_client.get(f"produto_brinquedos_jogos:{product_id}")
+
+        if product_data:
+            logger.info(msg="Produto retornado do Redis!")
+            return json.loads(product_data)
+
+
+        products = db.query(Product_Brinquedos_Jogos).filter(Product_Brinquedos_Jogos.id == product_id).first()
+        
+
+        if products:
+            logger.info(msg="Produto encontrado no banco de dados!")
+            product = Product_Brinquedos_Jogos.from_orm(products)
+
+            product_data = {
+                "id": products.id,
+                "name": products.name,
+                "description": products.description,
+                "price": products.price,
+                "quantity": products.quantity,
+                "tax": products.tax,
+                "stars": products.stars,
+                "color": products.color,
+                "size": products.size,
+                "details": products.details,
+                "category": 'Brinquedos_Jogos'
+            }
+            logger.info(msg="Produto inserido no redis!")
+            # Armazena no Redis com um tempo de expiração de 15 horas (54000 segundos)
+            redis_client.setex(f"produto_brinquedos_jogos:{products.id}", 54000, json.dumps(product_data))
+            logger.info(msg="Produto armazenado no Redis com expiração de 15 horas.")
+            # retorna do db
+            return product
+        
+        if not products:
+            logger.info(msg="Produto nao encontrado!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
+        
+
+    @staticmethod
+    async def delete_product_ID(product_id, db):
+        product_delete = db.query(Product_Brinquedos_Jogos).filter(Product_Brinquedos_Jogos.id == product_id).first()
+        
+        if product_delete:
+            logger.info(msg="Produto encontrado!")
+            db.delete(product_delete)
+            db.commit()
+            print("Produto deletado!!")
+            return f"Product with {product_id} removed succesfull"
+        
+        if product_delete is None:
+            logger.info(msg="Produto nao encontrado!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
+    
+
+    @staticmethod
+    async def update_product_ID(product_id, product_data ,db):
+        product = db.query(Product_Brinquedos_Jogos).filter(Product_Brinquedos_Jogos.id == product_id).first()
+    
+        if product:
+            # Atualiza os campos do produto com os dados recebidos
+            for key, value in product_data.dict().items():
+                setattr(product, key, value)
+
+            # Corrige o valor da categoria se necessário
+            #product.category = "Beleza_e_cuidados"  
+
+            # Salva as alterações no banco de dados
+            db.commit()
+            db.refresh(product)
+            return product
+
+        
+        # Verifica se o produto existe
+        if product is None:
+            logger.info(msg="Produto nao encontrado!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
+        
