942ab090ae7d186a37e07dd44c40ec5354818bee
Kelven <kelvenr40@gmail.com>
2025-03-27 13:32:03 -0300

    add service in controllers

--- ecommerce/controllers/routes_ecommrece/LivrosPapelaria/route.py
+++ ecommerce/controllers/routes_ecommrece/LivrosPapelaria/route.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import Session
 import json
 import uuid
 
+from ecommerce.controllers.services.services_livraria import ServicesLivraria
+
 
 route_livros_papelaria = APIRouter()
 
@@ -23,13 +25,7 @@ async def create_product(
     product: ProductLivrosPapelaria,
     db: Session = Depends(get_db)
 ):
-    product_id = str(uuid.uuid4())
-
-    db_product = Product_Livros_Papelaria(id=product_id, **product.dict())
-    db.add(db_product)
-    db.commit()
-    db.refresh(db_product)
-    return db_product
+    return await ServicesLivraria.create_product(product, db)
 
 
 
@@ -46,16 +42,7 @@ async def get_products(
     limit: int = 20,
     db: Session = Depends(get_db)
 ):
-    db_product = db.query(Product_Livros_Papelaria).offset(skip).limit(limit).all()
-    
-    if db_product:
-        logger.info(msg="Produtos de papelaria sendo listado!")
-        products_listed = [Product_Livros_Papelaria.from_orm(product) for product in db_product]
-        return products_listed
-    
-    if not db_product:
-        logger.info(msg="Nenhum produto de papelaria inserido!")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto de papelaria inserido!")
+    return await ServicesLivraria.get_products(skip, limit, db)
 
 
 # rota de filtragem de buscas 
@@ -66,7 +53,7 @@ async def get_products(
     description="List search products",
     name="Route search products"
 )
-def read_products(
+async def read_products(
     category: str = Query(None, description="Filtrar por categoria"),
     min_price: float = Query(None, description="Filtrar por preço mínimo"),
     max_price: float = Query(None, description="Filtrar por preço máximo"),
@@ -79,46 +66,10 @@ def read_products(
     limit: int = 10,
     db: Session = Depends(get_db)
 ):
-    query = db.query(Product_Livros_Papelaria)
-
-    # Aplicar filtros se fornecidos
-    # explicacao: ecommerce/databases/ecommerce_config/database.py -> linha 80
-    if name:
-        query = query.filter(Product_Livros_Papelaria.name.ilike(f"%{name}%"))
-
-    if category:# Usando LIKE para categoria
-        query = query.filter(Product_Livros_Papelaria.category.ilike(f"%{category}%")) 
-
-    if stars:# Usando LIKE para stars
-        query = query.filter(Product_Livros_Papelaria.stars >= stars)
-    
-    if color: # Usando LIKE para cor
-        query = query.filter(Product_Livros_Papelaria.color.ilike(f"%{color}%")) 
-
-    if details: # Usando LIKE para detalhes
-        query = query.filter(Product_Livros_Papelaria.details.ilike(f"%{details}%"))
-
-    if size:# Usando LIKE para tamanho
-        query = query.filter(Product_Livros_Papelaria.size.ilike(f"%{size}%"))  
-
-
-    if min_price is not None:
-        query = query.filter(Product_Livros_Papelaria.price >= min_price)
-
-    if max_price is not None:
-        query = query.filter(Product_Livros_Papelaria.price <= max_price)
-
-    
-    products = query.offset(skip).limit(limit).all()  # Usando o modelo SQLAlchemy
-
-    if products:
-        logger.info(msg="Produtos de papelaria sendo listados!")
-        products_listed = [Product_Livros_Papelaria.from_orm(product) for product in products]
-        return products_listed
-
-    logger.info(msg="Nenhum produto de papelaria encontrado!")
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto papelaria encontrado!")
-
+    return await ServicesLivraria.search_product(
+        db, name, category, stars, color,
+        details, size, min_price, max_price, skip, limit
+    )
 
 
 @route_livros_papelaria.get(
@@ -134,42 +85,7 @@ async def get_product_id(
     db: Session = Depends(get_db)
 ):
     
-    product_data = redis_client.get(f"produto_livraria:{product_id}")
-
-    if product_data:
-        logger.info(msg="Produto retornado do Redis!")
-        return json.loads(product_data)
-    
-    db_product = db.query(Product_Livros_Papelaria).filter(Product_Livros_Papelaria.id == product_id).first()
-    
-    if db_product:
-        logger.info(msg="Produto encontrado no banco de dados!")
-        product = Product_Livros_Papelaria.from_orm(db_product)
-
-        product_data = {
-            "id": db_product.id,
-            "name": db_product.name,
-            "description": db_product.description,
-            "price": db_product.price,
-            "quantity": db_product.quantity,
-            "tax": db_product.tax,
-            "stars": db_product.stars,
-            "color": db_product.color,
-            "size": db_product.size,
-            "details": db_product.details,
-            "category": "Livros_Papelaria"
-        }
-        logger.info(msg="Produto inserido no redis!")
-        # Armazena no Redis com um tempo de expiração de 15 horas (54000 segundos)
-        redis_client.setex(f"produto_livraria:{db_product.id}", 54000, json.dumps(product_data))
-        logger.info(msg="Produto armazenado no Redis com expiração de 15 horas.")
-        # retorna do db
-        return product
-
-    if db_product is None:
-        logger.info(msg="Produto nao encontrado!")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
-
+    return await ServicesLivraria.get_product_ID(product_id, db)
 
 
 @route_livros_papelaria.delete(
@@ -183,18 +99,7 @@ async def delete_product_id(
     product_id: str,
     db: Session = Depends(get_db)
 ):
-    product_delete = db.query(Product_Livros_Papelaria).filter(Product_Livros_Papelaria.id == product_id).first()
-    
-    if product_delete:
-        logger.info(msg="Produto encontrado!")
-        db.delete(product_delete)
-        db.commit()
-        print("Produto deletado!!")
-        return f"Product with {product_id} removed succesfull"
-
-    if product_delete is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
-
+    return await ServicesLivraria.delete_product(product_id, db)
 
 
 @route_livros_papelaria.put(
@@ -209,21 +114,4 @@ async def update_product(
     db: Session = Depends(get_db),
     product_data: ProductBase = Body(embed=True),
 ):
-    product = db.query(Product_Livros_Papelaria).filter(Product_Livros_Papelaria.id == product_id).first()
-
-    if product:
-        for key, value in product_data.dict().items():
-            setattr(product, key, value)
-
-        # Corrige o valor da categoria se necessário
-        #product.category = "Livros_Papelaria"  
-
-        # Salva as alterações no banco de dados
-        db.commit()
-        db.refresh(product)
-        return product
-
-
-    if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
-    
+    return await ServicesLivraria.update_product(product_id, db, product_data)
---
+++ ecommerce/controllers/services/services_livraria.py
@@ -0,0 +1,153 @@
+from fastapi import HTTPException, status
+from ecommerce.databases.ecommerce_config.database import get_db, redis_client
+from ecommerce.models.ecommerce.models import Product_Livros_Papelaria
+from ecommerce.config.config import logger
+import uuid
+import json
+
+
+
+class ServicesLivraria:
+
+
+    @staticmethod
+    async def create_product(product, db):
+        product_id = str(uuid.uuid4())
+
+        db_product = Product_Livros_Papelaria(id=product_id, **product.dict())
+        db.add(db_product)
+        db.commit()
+        db.refresh(db_product)
+        return db_product
+    
+    @staticmethod
+    async def get_products(skip, limit, db):
+        db_product = db.query(Product_Livros_Papelaria).offset(skip).limit(limit).all()
+        
+        if db_product:
+            logger.info(msg="Produtos de papelaria sendo listado!")
+            products_listed = [Product_Livros_Papelaria.from_orm(product) for product in db_product]
+            return products_listed
+        
+        if not db_product:
+            logger.info(msg="Nenhum produto de papelaria inserido!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto de papelaria inserido!")
+        
+
+    @staticmethod
+    async def search_product(
+        db, name, category, stars, color,
+        details, size, min_price, max_price, skip, limit
+    ):
+        query = db.query(Product_Livros_Papelaria)
+
+        # Aplicar filtros se fornecidos
+        # explicacao: ecommerce/databases/ecommerce_config/database.py -> linha 80
+        if name:
+            query = query.filter(Product_Livros_Papelaria.name.ilike(f"%{name}%"))
+
+        if category:# Usando LIKE para categoria
+            query = query.filter(Product_Livros_Papelaria.category.ilike(f"%{category}%")) 
+
+        if stars:# Usando LIKE para stars
+            query = query.filter(Product_Livros_Papelaria.stars >= stars)
+        
+        if color: # Usando LIKE para cor
+            query = query.filter(Product_Livros_Papelaria.color.ilike(f"%{color}%")) 
+
+        if details: # Usando LIKE para detalhes
+            query = query.filter(Product_Livros_Papelaria.details.ilike(f"%{details}%"))
+
+        if size:# Usando LIKE para tamanho
+            query = query.filter(Product_Livros_Papelaria.size.ilike(f"%{size}%"))  
+
+
+        if min_price is not None:
+            query = query.filter(Product_Livros_Papelaria.price >= min_price)
+
+        if max_price is not None:
+            query = query.filter(Product_Livros_Papelaria.price <= max_price)
+
+        
+        products = query.offset(skip).limit(limit).all()  # Usando o modelo SQLAlchemy
+
+        if products:
+            logger.info(msg="Produtos de papelaria sendo listados!")
+            products_listed = [Product_Livros_Papelaria.from_orm(product) for product in products]
+            return products_listed
+
+        logger.info(msg="Nenhum produto de papelaria encontrado!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nenhum produto papelaria encontrado!")
+    
+    @staticmethod
+    async def get_product_ID(product_id, db):
+        product_data = redis_client.get(f"produto_livraria:{product_id}")
+
+        if product_data:
+            logger.info(msg="Produto retornado do Redis!")
+            return json.loads(product_data)
+        
+        db_product = db.query(Product_Livros_Papelaria).filter(Product_Livros_Papelaria.id == product_id).first()
+        
+        if db_product:
+            logger.info(msg="Produto encontrado no banco de dados!")
+            product = Product_Livros_Papelaria.from_orm(db_product)
+
+            product_data = {
+                "id": db_product.id,
+                "name": db_product.name,
+                "description": db_product.description,
+                "price": db_product.price,
+                "quantity": db_product.quantity,
+                "tax": db_product.tax,
+                "stars": db_product.stars,
+                "color": db_product.color,
+                "size": db_product.size,
+                "details": db_product.details,
+                "category": "Livros_Papelaria"
+            }
+            logger.info(msg="Produto inserido no redis!")
+            # Armazena no Redis com um tempo de expiração de 15 horas (54000 segundos)
+            redis_client.setex(f"produto_livraria:{db_product.id}", 54000, json.dumps(product_data))
+            logger.info(msg="Produto armazenado no Redis com expiração de 15 horas.")
+            # retorna do db
+            return product
+
+        if db_product is None:
+            logger.info(msg="Produto nao encontrado!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
+        
+    @staticmethod
+    async def delete_product(product_id, db):
+        product_delete = db.query(Product_Livros_Papelaria).filter(Product_Livros_Papelaria.id == product_id).first()
+    
+        if product_delete:
+            logger.info(msg="Produto encontrado!")
+            db.delete(product_delete)
+            db.commit()
+            print("Produto deletado!!")
+            return f"Product with {product_id} removed succesfull"
+
+        if product_delete is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
+        
+    @staticmethod
+    async def update_product(product_id, db, product_data):
+        product = db.query(Product_Livros_Papelaria).filter(Product_Livros_Papelaria.id == product_id).first()
+
+        if product:
+            for key, value in product_data.dict().items():
+                setattr(product, key, value)
+
+            # Corrige o valor da categoria se necessário
+            #product.category = "Livros_Papelaria"  
+
+            # Salva as alterações no banco de dados
+            db.commit()
+            db.refresh(product)
+            return product
+
+
+        if product is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto nao encontrado!")
+        
