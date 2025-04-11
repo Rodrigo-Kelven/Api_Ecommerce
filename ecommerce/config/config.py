0f65d77e9511e521952ace42570f57241ba4a6a4
Rodrigo_Kelven <92493747+Rodrigo-Kelven@users.noreply.github.com>
2025-04-10 23:21:01 -0300

    Update config.py

--- ecommerce/config/config.py
+++ ecommerce/config/config.py
@@ -79,13 +79,13 @@ def cors(app):
     ]
 
     app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Content-Type", "Authorization"],
-    expose_headers=["X-Custom-Header"],
-    max_age=3600,
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "PUT", "DELETE"],
+        allow_headers=["Content-Type", "Authorization"],
+        expose_headers=["X-Custom-Header"],
+        max_age=3600,
     )
 """
 Ao permitir todas as origens (allow_origins=["*"]), você deve ter cuidado,
